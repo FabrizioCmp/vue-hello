1318ce3a1d35ebc2909cadb1711b6fd853c457a4
FabrizioCmp <fabriziocampari01@gmail.com>
2022-11-21 15:34:10 +0100

    stampo testo e immagine al btn-click

--- js/main.js
+++ js/main.js
@@ -3,7 +3,19 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            message: 'Hello Vue!',
+            textClass: "",
+            ImgHand: "",
+            imgClass:"",
+        }
+    },
+    methods: {
+        btnClick: function () {
+            console.log("btn clicked");
+            this.textClass = "show_text";
+            this.imgHand = "imgs/hi.png";
+            this.imgClass = "show_img";
         }
     }
 }).mount('#app');
+
